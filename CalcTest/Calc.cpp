6249bda795555cd5c04e05e9af55a8048c31ced4
dhonghun.lee <dhonghun.lee@samsung.com>
2024-05-20 17:04:42 +0900

    [feature] getminus

--- CalcTest/Calc.cpp
+++ CalcTest/Calc.cpp
@@ -4,7 +4,9 @@ public:
 	int getSum(int a, int b);
 	int getGop(int a, int b);
 	int getZegop(int a);
-	int getMinus(int a, int b);
+	int getMinus(int a, int b) {
+		return a - b;
+	}
 	int getDivide(int a, int b);
 	int getSumSum(int a, int b, int c);
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -6,3 +6,9 @@ TEST(CalcTest, TestName) {
 	int sum = calc.getSum(1, 2);
 	EXPECT_EQ(sum, 3);
 }
+
+TEST(CalcTest, TestGetMinus) {
+	Calc calc;
+	int ret = calc.getMinus(2, 1);
+	EXPECT_EQ(ret, 1);
+}
